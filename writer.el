a387607bb0b922dd2cc992a0ad3eae8357197ed2
jmisciagno <johnmisciagno@gmail.com>
2025-07-14 01:06:44 -0400

    Add files via upload

--- writer.el
+++ writer.el
@@ -22,66 +22,35 @@
 
 (require 'org-bullets)
 (require 'org-variable-pitch)
-(setq org-bullets-bullet-list (list "" "" "" ""))
+(setq org-bullets-bullet-list (list "○" "" "" "")) ; Eliminate all Bullet points except for the first.
 
-(defun writer-mode-on ()
+(defun writer-mode-on (reset)
   (setq mode-line-format nil)
   (setq org-hide-emphasis-markers t)
   (org-indent-mode 1)
-  (font-lock-add-keywords 'org-mode
-                          '(("^ *\\([-]\\) "
-                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-  (org-bullets-mode 1)
-  (org-variable-pitch-minor-mode)
-  (let* ((variable-tuple (cond ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
-                               ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
-                               ((x-list-fonts "Verdana")         '(:font "Verdana"))
-                               ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
-                               (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
-	 (base-font-color     (face-foreground 'default nil 'default))
-	 (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
-    (custom-theme-set-faces 'user
-                            `(org-level-8 ((t (,@headline ,@variable-tuple))))
-                            `(org-level-7 ((t (,@headline ,@variable-tuple))))
-                            `(org-level-6 ((t (,@headline ,@variable-tuple))))
-                            `(org-level-5 ((t (,@headline ,@variable-tuple))))
-                            `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
-                            `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
-                            `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.35))))
-                            `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.5))))
-                            `(org-document-title ((t (,@headline ,@variable-tuple :height 1.5 :underline nil)))))
-    ))
+  (org-variable-pitch-minor-mode 1)
+  (font-lock-add-keywords nil '(("^ *\\([-]\\) " (0 (prog1 ()(compose-region (match-beginning 1) (match-end 1) "•"))))))
+  (let ((base-font-color (face-foreground 'default nil 'default)))
+    (face-remap-add-relative 'org-level-8 `(:weight bold :foreground ,base-font-color))
+    (face-remap-add-relative 'org-level-7 `(:weight bold :foreground ,base-font-color))
+    (face-remap-add-relative 'org-level-6 `(:weight bold :foreground ,base-font-color))
+    (face-remap-add-relative 'org-level-5 `(:weight bold :foreground ,base-font-color))
+    (face-remap-add-relative 'org-level-4 `(:weight bold :foreground ,base-font-color :height 1.1))
+    (face-remap-add-relative 'org-level-3 `(:weight bold :foreground ,base-font-color :height 1.25))
+    (face-remap-add-relative 'org-level-2 `(:weight bold :foreground ,base-font-color :height 1.35))
+    (face-remap-add-relative 'org-level-1 `(:weight bold :foreground ,base-font-color :height 1.5))
+    (face-remap-add-relative 'org-document-title `(:weight bold :foreground ,base-font-color :height 1.75 :underline nil)))
+  (org-bullets-mode 1) ; must be last
+  (when reset (org-mode)))
 
-
-(defun writer-mode-off ()
+(defun writer-mode-off (reset)
   (setq mode-line-format t)
   (setq org-hide-emphasis-markers nil)
   (org-indent-mode -1)
-  (remove-hook 'org-mode-hook #'writer-setup)
-  (org-bullets-mode -1)
   (org-variable-pitch-minor-mode -1)
-  (font-lock-remove-keywords 'org-mode
-                          '(("^ *\\([-]\\) "
-                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-  (custom-theme-set-faces 'user
-                          `(org-level-8 ((t :inherit outline-8)))
-                          `(org-level-7 ((t :inherit outline-7)))
-                          `(org-level-6 ((t :inherit outline-6)))
-                          `(org-level-5 ((t :inherit outline-5)))
-                          `(org-level-4 ((t :inherit outline-4)))
-                          `(org-level-3 ((t :inherit outline-3)))
-                          `(org-level-2 ((t :inherit outline-2)))
-                          `(org-level-1 ((t :inherit outline-1)))
-                          `(org-document-title ((((class color) (background light)) (:foreground "midnight blue" :weight bold))
-    (((class color) (background dark)) (:foreground "pale turquoise" :weight bold))) (t (:weight bold))))
-  (org-mode))
-
-;;; There are three minor modes enabled and showing in the mode line
-;; - buffer-face-mode " BufFace"
-;; - org-indent-mode " Ind"
-;; - org-variable-pitch-minor-mode " OVP"
-;; I need to get rid of these
-
+  (font-lock-remove-keywords nil '(("^ *\\([-]\\) " (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+  (org-bullets-mode -1) ; must be last
+  (when reset (org-mode)))
 
 (defun copy-as-rtf ()
   "Export region to RTF and copy it to the clipboard."
@@ -95,7 +64,7 @@
 		  (org-export-to-buffer 'html "Formatted Copy" nil nil t t)))
 	   (html (progn
 		   (setq org-export-with-toc toc)
-		   (setq org-export-with-section-numbers with-section-number)
+		   (setq org-export-with-section-numbers with-section-numbers)
 		   (with-current-buffer buf (buffer-string)))))
       (with-current-buffer buf
         (shell-command-on-region
@@ -107,39 +76,24 @@
 
 (defvar writer-mode-map
   (let ((map (make-sparse-keymap)))
-    (set-keymap-parent map org-mode-map) ; set parent
+    (set-keymap-parent map org-mode-map) ; set parent mode map
     (define-key map (kbd "s-c") #'copy-as-rtf)
-    map)
-  "Keymap for my extended minor mode.")
+    map))
 
 (define-minor-mode writer-mode
-  "Language mode documentation."
   :lighter " Writer")
 
+; Manage State
 (defun writer-mode-toggle ()
   (if (derived-mode-p 'org-mode)
-      (if writer-mode (writer-mode-on) (writer-mode-off))
+      (if writer-mode (writer-mode-on nil) (writer-mode-off t))
       (progn (princ "Failed to launch Writer mode.  Org mode must be active.")
 	     (ding))))
 
+(add-hook 'org-mode-hook (lambda () (if writer-mode (writer-mode-on t) (writer-mode-off nil))))
 (add-hook 'writer-mode-hook #'writer-mode-toggle) ; start or clean up
 
-;; Clean up mode-line
-(defun set-mode-line (m s)
-  (let ((res (assq m minor-mode-alist)))
-    (when res (setcar (cdr res) s))))
-
-(defun remove-from-mode-line (m m0 s)
-  (lambda (&rest args)
-    (if (bound-and-true-p m)
-	(set-mode-line m0 "")
-        (set-mode-line m0 s))))
-
 ;; Remove minor mode dependencies from modeline
-;; (advice-add 'buffer-face-mode :before (remove-from-mode-line writer-mode 'buffer-face-mode " BufFace"))
-;; (advice-add 'org-indent-mode :before (remove-from-mode-line writer-mode 'org-indent-mode " Ind"))
-;; (advice-add 'org-variable-pitch-minor-mode :before (remove-from-mode-line writer-mode 'org-variable-pitch-minor-mode " OVP"))
-
 (defun set-mode-line (m s)
   (let ((res (assq m minor-mode-alist)))
     (when res (setcar (cdr res) s))))
